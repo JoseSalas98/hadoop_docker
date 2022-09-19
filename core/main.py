3563669023582197bb585bc9a430f6d617f2cd6c
José Salas <josesalasbolivar98@gmail.com>
2022-09-18 22:42:54 -0500

    Add response time mapper and response time mean value calculator

--- core/main.py
+++ core/main.py
@@ -9,3 +9,10 @@ word_by_tag = mp_funct.word_by_tag_mapper()
 word_by_tag = mp_funct.word_by_tag_reducer(key_value_list=word_by_tag)
 top_word_by_tag = mp_funct.top_word_by_tag_calculator(
     key_value_list=word_by_tag)
+
+"""Response Time"""
+key_value_list = mp_funct.response_time_mapper()
+response_time_mean_value = mp_funct.response_time_mean_calculator(
+    key_value_list=key_value_list)
+print(
+    f"El tiempo promedio de respuesta para los posts es de {response_time_mean_value} segundos")
--- core/modules/map_reduce_function.py
+++ core/modules/map_reduce_function.py
@@ -1,6 +1,9 @@
-import re
+import dateutil.parser
+import datetime
 import os
 import operator
+import re
+import statistics as sts
 import lxml.html as LH
 import xml.etree.ElementTree as ET
 from pathlib import Path
@@ -19,7 +22,8 @@ def top_view_mapper() -> list:
     appends these values in a dictionary within a list of dictionaries.
 
     Returns:
-        list: List of dictionaries with format {"post_id": post_id, "view_count": view_count}. 
+        list: List of dictionaries with format {"post_id": post_id, 
+                                                "view_count": view_count}. 
     """
     mytree = ET.parse(f"{DATASETS_DIR}/posts.xml")
     # Instance of the root tree.
@@ -56,7 +60,8 @@ def top_view_calculator(key_value_list: list) -> object:
 
     Args:
         key_value_list (list): Sorted ascending list of dictionaries with format: 
-        {"post_id": post_id, "view_count": view_count}.
+        {"post_id": post_id, 
+         "view_count": view_count}.
 
     Returns:
         object: Text file with top 10 most viewed posts.
@@ -88,10 +93,12 @@ def word_by_tag_mapper() -> list:
 
     Returns:
         list: List of dictionaries sort descending by "post_tag" and "post_body_word" with format: 
-        {"post_tag": post_tag, "post_body_word": post_body_word, "count": count}
+        {"post_tag": post_tag, 
+         "post_body_word": post_body_word, 
+         "count": count}
     """
     mytree = ET.parse(f"{DATASETS_DIR}/posts.xml")
-    # instance "of" the root tree.
+    # Instance "of" the root tree.
     myroot = mytree.getroot()
     top_limit01 = len(myroot)
     KEY_VALUE_LIST = []
@@ -138,11 +145,15 @@ def word_by_tag_reducer(key_value_list: list) -> list:
 
     Args:
         key_value_list (list): List of dictionaries sort descending by "post_tag" and "post_body_word" with format: 
-        {"post_tag": post_tag, "post_body_word": post_body_word, "count": count}
+        {"post_tag": post_tag, 
+        "post_body_word": post_body_word, 
+        "count": count}
 
     Returns:
         list: List of dictionaries sort descending by "post_tag" and "count" with 
-        format: {"post_tag": post_tag, "post_body_word": post_body_word, "count": count}
+        format: {"post_tag": post_tag, 
+                 "post_body_word": post_body_word, 
+                 "count": count}
     """
     WORD_BY_CATEGORY = []
     current_word = None
@@ -188,7 +199,9 @@ def top_word_by_tag_calculator(key_value_list: list) -> object:
 
     Args:
         key_value_list (list): List of dictionaries sort descending by "post_tag" and "count" with 
-        format: {"post_tag": post_tag, "post_body_word": post_body_word, "count": count}
+        format: {"post_tag": post_tag, 
+        "post_body_word": post_body_word, 
+        "count": count}
 
     Returns:
         object: Text file with top 10 most repeated words by tag.
@@ -250,4 +263,194 @@ def top_word_by_tag_calculator(key_value_list: list) -> object:
         file.close()
 
 
-"""Top 10 most viwed post"""
+"""Response time"""
+
+
+def datetime_obj(datetime_str: str) -> datetime.date:
+    # Convert to datetime object
+    some_datetime_obj = dateutil.parser.parse(datetime_str)
+
+    # Get Year, Month, and Day as integers
+    year = some_datetime_obj.year
+    month = some_datetime_obj.month
+    day = some_datetime_obj.day
+    hour = some_datetime_obj.hour
+    min = some_datetime_obj.minute
+    sec = some_datetime_obj.second
+
+    # Define dateformat
+    date_format = "{year}/{month:02d}/{day:02d} {hour}:{min}:{sec}"
+    date_string = date_format.format(year=year, month=month,
+                                     day=day, hour=hour,
+                                     min=min, sec=sec)
+
+    # Caculate date
+    format = "%Y/%m/%d %H:%M:%S"
+    date = datetime.datetime.strptime(date_string, format)
+
+    return date
+
+
+def response_time_mapper() -> list:
+    """This function reads a .xml file, searches for "post_id", "post_score", "post_type", "parent_id",
+    "accepted_answer_id" and"creation_date" to appends these values in a dictionary within a list of dictionaries.
+
+    Returns:
+        list: List of dictionaries sort descending by "post_type" and "score" with
+        format: {"id": id, 
+                 "score": score, 
+                 "post_type": post_type, 
+                 "parent_id": parent_id, 
+                 "accepted_answer_id": accepted_answer_id, 
+                 "creation_date": creation_date}
+    """
+    mytree = ET.parse(f"{DATASETS_DIR}/posts.xml")
+    # Instance "of" the root tree
+    myroot = mytree.getroot()
+    KEY_VALUE_LIST = []
+    min_rank = 199
+    max_rank = 299
+    id = None
+    score = None
+    post_type = None
+    parent_id = None
+    accepted_answer_id = None
+    creation_date = None
+
+    for row in range(min_rank, max_rank):
+        # Finding elementes.
+        id = int(myroot[row].attrib["Id"])
+        score = int(myroot[row].attrib["Score"])
+        post_type = int(myroot[row].attrib["PostTypeId"])
+        creation_date = (myroot[row].attrib["CreationDate"])
+        creation_date = datetime_obj(creation_date)
+
+        try:
+            accepted_answer_id = int(myroot[row].attrib["AcceptedAnswerId"])
+        except KeyError:
+            accepted_answer_id = None
+
+        try:
+            parent_id = int(myroot[row].attrib["ParentId"])
+        except KeyError:
+            parent_id = None
+
+        # Append finded elements to dictionary within list of dictionaries.
+        KEY_VALUE_LIST.append({"id": id, "score": score, "post_type": post_type, "parent_id": parent_id,
+                               "accepted_answer_id": accepted_answer_id, "creation_date": creation_date})
+
+    # Sort list by "post_type", "score".
+    KEY_VALUE_LIST = sorted(
+        KEY_VALUE_LIST, key=operator.itemgetter("post_type", "score"), reverse=False)
+
+    return KEY_VALUE_LIST
+
+
+def response_time_mean_calculator(key_value_list: list) -> int:
+    """This function iter throuht key_value_list and .xml file to calculate the response time mean value (sec)
+    betwent the posts and their respective answer.
+
+    Args:
+        key_value_list (list): List of dictionaries sort descending by "post_type" and "score" with
+        format: {"id": id, 
+                 "score": score, 
+                 "post_type": post_type, 
+                 "parent_id": parent_id, 
+                 "accepted_answer_id": accepted_answer_id, 
+                 "creation_date": creation_date}
+
+    Returns:
+        int: Response time mean value (sec)
+    """
+    mytree = ET.parse(f"{DATASETS_DIR}/posts.xml")
+    # Instance "of" the root tree
+    myroot = mytree.getroot()
+    top_limit01 = len(myroot)
+    match_value = None
+    post_id = None
+    answer_id = None
+    parent_id = None
+    post_creation_date = None
+    response_time = None
+    RESPONSE_TIME_LIST = []
+    mean = None
+    MEAN = []
+
+    for dictionary in key_value_list:
+
+        # Find elementes.
+        post_id = dictionary["id"]
+        # Validate post type: question
+        if dictionary["post_type"] == 1:
+            # Find the creation date and respective answer.
+            post_creation_date = dictionary["creation_date"]
+            answer_id = dictionary["accepted_answer_id"]
+            # If the answer exists then validate the match between the post (question)
+            # in the list of dictionaries and the .xml file id attribute (match value) through answer_id.
+            if answer_id:
+                for row in range(0, top_limit01):
+                    match_value = int(myroot[row].attrib["Id"])
+                    # If there is match append the find values to the list of dictionaries
+                    if match_value == answer_id:
+                        answer_creation_date = (
+                            myroot[row].attrib["CreationDate"])
+                        # Transform datetime xml string to datetime object
+                        answer_creation_date = datetime_obj(
+                            answer_creation_date)
+                        # Calculate the difference between answer_creation_date and post_creation_date
+                        # (response time) and return a time delta object. It's necessary to convert it to seconds and then to integer.
+                        response_time = (answer_creation_date -
+                                         post_creation_date).seconds
+                        response_time = int(response_time)
+                        # Append the find values to the list of dictionaries.
+                        RESPONSE_TIME_LIST.append(
+                            {"post_id": post_id,
+                             "post_creation_date": post_creation_date,
+                             "answer_id": answer_id,
+                             "answer_creation_date": answer_creation_date,
+                             "response_time(seg)": response_time}
+                        )
+                        # Append the response_time to the MEAN list.
+                        MEAN.append(response_time)
+                        break
+            else:
+                # If there is no answer append the None values to the list of dictionaries
+                wth_answer = None
+                RESPONSE_TIME_LIST.append(
+                    {"post_id": post_id,
+                     "post_creation_date": post_creation_date,
+                     "answer_id": wth_answer,
+                     "answer_creation_date": wth_answer,
+                     "response_time": wth_answer}
+                )
+        # Validate post type: answer
+        elif dictionary["post_type"] == 2:
+            parent_id = dictionary["parent_id"]
+            answer_creation_date = dictionary["creation_date"]
+            for row in range(0, top_limit01):
+                match_value = int(myroot[row].attrib["Id"])
+                # Validate the match between the post (answer)
+                # in the list of dictionaries and the .xml file id attribute (match value) through parent_id.
+                # If there is match append the find values to the list of dictionaries
+                if match_value == parent_id:
+                    post_creation_date = (myroot[row].attrib["CreationDate"])
+                    post_creation_date = datetime_obj(post_creation_date)
+                    # Calculate the difference between answer_creation_date and post_creation_date
+                    # (response time) and return a time delta object. It's necessary to convert it to seconds and then to integer.
+                    response_time = (answer_creation_date -
+                                     post_creation_date).seconds
+                    response_time = int(response_time)
+                    RESPONSE_TIME_LIST.append(
+                        {"post_id": post_id,
+                         "post_creation_date": post_creation_date,
+                         "answer_id": answer_id,
+                         "answer_creation_date": answer_creation_date,
+                         "response_time(seg)": response_time}
+                    )
+                    # Append the response_time to the MEAN list.
+                    MEAN.append(response_time)
+                    break
+
+    # Calculate response time mean value.
+    mean = round(sts.mean(MEAN), 2)
+    return mean
